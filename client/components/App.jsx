8b5d8473ea8e89ed5e3301b48fdb3c6450e72f54
shymala <shymala.sidharth22@gmail.com>
2022-11-11 13:00:37 +1300

    updated work

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,27 +1,52 @@
 import React, { useState, useEffect } from 'react'
-import { fetchFruits } from '../apiClient'
+import { fetchProfiles } from '../apis/profiles'
+
+// import Form from './Form'
 
 const App = () => {
-  const [profiles, setProfiles] = useState({})
+  const [profiles, setProfiles] = useState([])
 
-  // useEffect(() => {
-  //   fetchFruits()
-  //     .then((fruits) => {
-  //       setFruits(() => fruits)
-  //     })
-  //     .catch((err) => {
-  //       console.error(err)
-  //     })
-  // }, [])
+  useEffect(() => {
+    fetchProfiles()
+      .then((profiles) => {
+        console.log(profiles)
+        setProfiles(() => profiles)
+      })
+      .catch((err) => console.log(err.message))
+  }, [])
 
   return (
-    // <main className="bg-slate-700 h-screen flex justify-center items-center text-white text-4xl">
-    //   <ul>
-    //     {fruits.map((fruit) => (
-    //       <li key={fruit.id}>{fruit.name}</li>
-    //     ))}
-    //   </ul>
-    // </main>
+    <section className="formSection">
+      <div className="form">
+        <h2>Profiles!</h2>
+        <input type="text" name="name" placeholder="Name" required={true} />
+        <input
+          type="text"
+          name="pronouns"
+          placeholder="Pronouns"
+          required={true}
+        />
+        <input
+          type="text"
+          name="interests"
+          placeholder="Interests"
+          required={true}
+        />
+        <input
+          type="image"
+          alt="photo"
+          name="photo"
+          placeholder="Upload Image"
+          required={true}
+        />
+        <input
+          type="text"
+          name="zodiac sign"
+          placeholder="Zodiac"
+          required={true}
+        />
+      </div>
+    </section>
   )
 }
 
